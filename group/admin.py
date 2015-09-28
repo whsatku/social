6a1e5010f067d890836d6a483824d939c3c66e94
worapon <worapon.o@ku.th>
2015-09-28 22:17:35 +0700

    add admin group and group member

---
+++ group/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from models import *
+
+
+class GroupAdmin(admin.ModelAdmin):
+    list_display = (
+    	'name', 'type', 'description', 'long_description',
+		'logo', 'header', 'permisssion')
+
+class GroupMemberAdmin(admin.ModelAdmin):
+    list_display = (
+    	'group_id','user_id', 'role')
+
+
+admin.site.register(Group, GroupAdmin)
+admin.site.register(GroupMember, GroupMemberAdmin)
