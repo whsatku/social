b6db9bdd276774831ae372dc1f0ceccda1d495d9
yoyothai00 <yoyothai00@gmail.com>
2015-10-04 15:22:19 +0700

    Web able to show newsfeed which come from db

--- ui/static/app/newsfeed.js
+++ ui/static/app/newsfeed.js
@@ -2,8 +2,13 @@
 
 var app = angular.module('app.newsfeed', []);
 
-app.controller('NewsfeedController', function($scope){
+app.controller('NewsfeedController', function($scope, $http){
 	$scope.allowPost = true;
+	$scope.newsfeed = null;
+	$http.get('/api/newsfeed/post').success(function(data){
+		$scope.newsfeed = data;
+		console.log($scope.newsfeed);
+	})
 });
 
 })();
