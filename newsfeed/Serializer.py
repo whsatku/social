3b28d0fdd899ed439a0ce8ecaffb2c574e4f09cf
Best <kazixma@gmail.com>
2015-10-04 11:07:02 +0700

    fix user

--- newsfeed/Serializer.py
+++ newsfeed/Serializer.py
@@ -1,13 +1,23 @@
 from django.contrib.auth.models import User
+from rest_framework import serializers
 
 from rest_framework.serializers import HyperlinkedModelSerializer, ModelSerializer
 from newsfeed.models import Post, Comment
 
 
 class PostSerializer(ModelSerializer):
+
+    #user = serializers.SerializerMethodField('_user')
+
+    # Use this method for the custom field
+    def _user(self, obj):
+        users = self.context['request'].user
+        return users
+
     class Meta:
         model = Post
-        fields = ('id', 'text', 'date', 'target_type', 'target_id', 'target_object')
+        fields = ('id', 'user','text', 'date', 'target_type', 'target_id', 'target_object')
+        #read_only_fields = ('user',)
 
 
 class CommentSerializer(ModelSerializer):
--- newsfeed/views.py
+++ newsfeed/views.py
@@ -1,14 +1,17 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render
 
 # Create your views here.
 from rest_framework import status
+from rest_framework.exceptions import NotAuthenticated
+from rest_framework.generics import ListCreateAPIView
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from newsfeed.Serializer import PostSerializer, CommentSerializer
 from newsfeed.models import Post
 
 
-class PostView(APIView):
+class PostView(ListCreateAPIView):
     serializer_class = PostSerializer
 
     def get(self, request, id=None, format=None):
@@ -18,11 +21,14 @@ class PostView(APIView):
         return Response(response.data)
 
     def post(self, request, format=None):
+
         serializer = PostSerializer(data=request.data)
+
         if serializer.is_valid():
-            serializer.save()
+
+           # serializer.user = self.request.user
+            serializer.save(user=User.objects.get(id=self.request.user.id))
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
 
 
