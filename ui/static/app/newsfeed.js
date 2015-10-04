39287276402f2d2406e876feb6ae7b7e13b5e54f
yoyothai00 <yoyothai00@gmail.com>
2015-10-04 15:23:30 +0700

    remove debug code

--- ui/static/app/newsfeed.js
+++ ui/static/app/newsfeed.js
@@ -7,7 +7,6 @@ app.controller('NewsfeedController', function($scope, $http){
 	$scope.newsfeed = null;
 	$http.get('/api/newsfeed/post').success(function(data){
 		$scope.newsfeed = data;
-		console.log($scope.newsfeed);
 	})
 });
 
