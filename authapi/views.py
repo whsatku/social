673c8d030e7f28da151c32cb4da9d9494b10e2d2
Manatsawin Hanmongkolchai <manatsawin+git@gmail.com>
2015-10-04 16:31:13 +0700

    KU Login

--- authapi/views.py
+++ authapi/views.py
@@ -1,12 +1,13 @@
+from django.contrib.auth import authenticate, login
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.exceptions import NotAuthenticated
+from rest_framework.exceptions import NotAuthenticated, ValidationError, PermissionDenied
 
 class UserViewSet(APIView):
 	"""Validate current user session
 
-	This API is used to check whether the current user is logged in
-	and to retrieve information about the user.
+	Check whether the current user is logged in and retrieve
+	information about the user.
 
 	It could be accessed at :http:get:`/api/auth/check`
 
@@ -24,3 +25,31 @@ class UserViewSet(APIView):
 			return Response(request.user.get_username())
 		else:
 			raise NotAuthenticated()
+
+class LoginViewSet(APIView):
+	"""Log a user in with username/password combination
+
+	It could be accessed at :http:post:`/api/auth/login`
+
+	"""
+	def post(self, request):
+		"""Log a user in
+
+		Args:
+			request: Django Rest Framework request object
+
+		Return:
+			Username or 403
+		"""
+		if 'username' not in request.data or 'password' not in request.data:
+			raise ValidationError('Specify username and password')
+
+		user = authenticate(username=request.data['username'], password=request.data['password'])
+
+		if not user:
+			raise PermissionDenied('Cannot log you in')
+		if not user.is_active:
+			raise PermissionDenied('User disabled')
+
+		login(request, user)
+		return Response(user.get_username())
