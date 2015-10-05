a967095959e2ab3938f8b347c23757e7fc4127e9
Supason Kotanut <supasonk@gmail.com>
2015-10-05 13:58:57 +0700

    add import

--- group/views.py
+++ group/views.py
@@ -1,5 +1,5 @@
 from rest_framework.response import Response
-from rest_framework.generics import ListCreateAPIView
+from rest_framework.generics import ListCreateAPIView , APIView
 from rest_framework.exceptions import ValidationError, NotAuthenticated, NotFound
 from models import *
 from serializers import *
