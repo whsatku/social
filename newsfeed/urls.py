01cabf0367a8231e9524adb041b0ab413237f5fd
Perawith <perawith.j@ku.th>
2015-10-05 17:31:57 +0700

    Add comment

--- newsfeed/urls.py
+++ newsfeed/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     url(r'^post/$', views.PostViewList.as_view()),
     url(r'^post/(?P<id>\d+)/$', views.PostViewDetail.as_view()),
+    url(r'^comment/$', views.CommentViewList.as_view()),
+    url(r'^comment/(?P<id>\d+)/$', views.CommentViewDetail.as_view()),
 ]
--- newsfeed/views.py
+++ newsfeed/views.py
@@ -5,7 +5,9 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from newsfeed.models import Post
+from newsfeed.models import Comment
 from newsfeed.serializer import PostSerializer
+from newsfeed.serializer import CommentSerializer
 
 
 class PostViewList(APIView):
@@ -47,4 +49,45 @@ class PostViewDetail(APIView):
     def delete(self, request, id, format=None):
         posts = self.get_object(id)
         posts.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class CommentViewList(APIView):
+    serializer_class = CommentSerializer
+
+    def get(self, request, id=None, format=None):
+        comment = Comment.objects.all()
+        response = self.serializer_class(comment, many=True)
+
+        return Response(response.data)
+
+    def post(self, request, format=None):
+        serializer = CommentSerializer(data=request.data)
+
+        if serializer.is_valid():
+            # serializer.user = self.request.user
+            serializer.save(user=User.objects.get(id=self.request.user.id))
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class CommentViewDetail(APIView):
+    serializer_class = CommentSerializer
+
+    def get_object(self, id):
+        try:
+            return Comment.objects.get(id=id)
+        except Comment.DoesNotExist:
+            raise Http404
+
+    def get(self, request, id=None, format=None):
+
+        comment_object = self.get_object(id)
+        response = self.serializer_class(comment_object)
+
+        return Response(response.data)
+
+    def delete(self, request, id, format=None):
+        comments = self.get_object(id)
+        comments.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
