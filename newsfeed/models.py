62cbf6be4c4ff16e536437515820dadcf9becdf7
Sorrawit Chancherngkit <ongoing@outlook.com>
2015-10-07 10:58:08 +0700

    edit Post/Comment model's Date to DateTime, postToNewsfeed target_id = -1

--- newsfeed/models.py
+++ newsfeed/models.py
@@ -7,7 +7,7 @@ from django.contrib.contenttypes.models import ContentType
 class Post(models.Model):
     user = models.ForeignKey(User, default=0)
     text = models.CharField(max_length=2000)
-    date = models.DateField(auto_now_add=True)
+    date = models.DateTimeField(auto_now_add=True)
     target_type = models.ForeignKey(ContentType)
     target_id = models.PositiveIntegerField()
     target_object = GenericForeignKey('target_type', 'target_id')
@@ -20,7 +20,7 @@ class Comment(models.Model):
     post = models.ForeignKey(Post)
     user = models.ForeignKey(User)
     text = models.CharField(max_length=2000)
-    date = models.DateField(auto_now_add=True)
+    date = models.DateTimeField(auto_now_add=True)
 
     def __unicode__(self):
         return "{}'s comment (id={})".format(self.user.username, self.id)
