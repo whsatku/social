b69bf6ab5ebf4ad3c66e20b525e5e3266b450bc0
Supason Kotanut <supasonk@gmail.com>
2015-10-12 17:13:55 +0700

    fixed group models

--- group/models.py
+++ group/models.py
@@ -6,14 +6,17 @@ from django.contrib.auth.models import User
 
 class Group(models.Model):
     name = models.CharField(max_length=25)
-    type =  models.IntegerField(default=0)
-    description = models.CharField(max_length=50)
-    long_description = models.CharField(max_length=200)
+    type = models.IntegerField(default=0)
+    category = models.CharField(max_length=25)
+    description = models.CharField(max_length=200)
+    short_description = models.CharField(max_length=50)
+    activities = models.CharField(max_length=200)
     logo = models.CharField(max_length=25)
     #logo_image = ImageField(upload_to=get_image_path, blank=True, null=True)
     header = models.CharField(max_length=25)
     #header_image = ImageField(upload_to=get_image_path, blank=True, null=True)
     permisssion = models.IntegerField(default=0)
+
 
 class GroupMember(models.Model):
     group = models.ForeignKey(Group, default=0)
