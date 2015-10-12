3ea3b90f379f93b5482ed081178eba90c8408921
Sorrawit Chancherngkit <ongoing@outlook.com>
2015-10-13 01:34:46 +0700

    Show humanized relative time instead of full-format DateTime

--- newsfeed/models.py
+++ newsfeed/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
+from django.contrib.humanize.templatetags.humanize import naturaltime
 
 
 class Post(models.Model):
@@ -12,6 +13,9 @@ class Post(models.Model):
     target_id = models.PositiveIntegerField()
     target_object = GenericForeignKey('target_type', 'target_id')
 
+    def FORMAT(self):
+        return naturaltime(self.datetime)
+
     def __unicode__(self):
         return "{}'s newsfeed (id={})".format(self.user.username, self.id)
 
@@ -21,6 +25,9 @@ class Comment(models.Model):
     user = models.ForeignKey(User)
     text = models.CharField(max_length=2000)
     datetime = models.DateTimeField(auto_now_add=True)
+
+    def FORMAT(self):
+        return naturaltime(self.datetime)
 
     def __unicode__(self):
         return "{}'s comment (id={})".format(self.user.username, self.id)
