6cfed42c9b47ec81fb6fabfee1677a6cda84b641
worapon <worapon.o@ku.th>
2015-10-27 01:38:45 +0700

    create user proflie after first login by facebook

--- User/models.py
+++ User/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.dispatch import receiver
+
+from allauth.account.signals import user_signed_up
+from allauth.socialaccount.models import SocialAccount
 
 
 GENDER = (
@@ -30,3 +34,8 @@ class UserProfile(models.Model):
 
     class Meta:
         db_table = 'youniversity_profile'
+
+@receiver(user_signed_up)
+def save_after_sign_up(sender, user, sociallogin=None, **kwargs):
+    new_user_id = user.id
+    profile = UserProfile.objects.create(user=user)
