b174118f7995b1cd69db0090f7166b66d09f60df
Supason Kotanut <supasonk@gmail.com>
2015-11-21 12:57:33 +0700

    implement add people to event api

--- event/views.py
+++ event/views.py
@@ -53,3 +53,39 @@ class EventViewSet(APIView):
             return Event.objects.get(id=event_id)
         except Event.DoesNotExist:
             raise Http404
+
+
+class EventDetail(APIView):
+
+    def get_member(self, event_id, user_id):
+        """Get user from group's database.
+
+        Args:
+                request: Django Rest Framework request object
+                group_id: ID of group
+                format: pattern for Web APIs
+
+        Return:
+
+        """
+        try:
+            return EventMember.objects.get(event=event_id, user=user_id)
+        except GroupMember.DoesNotExist:
+            raise Http404
+
+    def put(self, request, event_id, pk, format=None):
+        """Add or update member in group.
+
+        Args:
+                request: Django Rest Framework request object
+                event_id: ID of event
+                pk: ID of user
+                format: pattern for Web APIs
+
+        Return:
+
+        """
+        member = self.get_member(event_id, pk)
+        member.role = 1
+        member.save()
+        return Response(status=status.HTTP_201_CREATED)
