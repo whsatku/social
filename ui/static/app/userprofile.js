e4a0f1c3beff67b7117ad19d3057190c136bfb7d
Sorrawit Chancherngkit <ongoing@outlook.com>
2015-11-30 19:18:22 +0700

    add FriendsCount to UserTimeline

--- ui/static/app/userprofile.js
+++ ui/static/app/userprofile.js
@@ -32,8 +32,10 @@ app.controller('UserProfileInfoController', function($scope, $http, $location, $
 });
 
 app.controller('UserFriendController', function($scope, $http, $location, $stateParams){
-    $http.get('/api/user/friends').success(function(data){
+    var userID = $stateParams.user;
+    $http.get('/api/user/friends/' + userID).success(function(data){
       $scope.friends = data;
+      $scope.friendsCount = data.length;
     });
     $scope.unFriend = function(otherUserId){
     	$http.delete('/api/user/friend/' + otherUserId ).success(function(data){
@@ -45,6 +47,11 @@ app.controller('AddFriendController', function($scope, $http, $location, $stateP
     $scope.userId = $rootScope.user.id;
     $scope.otherUserId = $stateParams.user;
     $scope.isFriend = false;
+
+    $http.get('/api/user/friends/' + $scope.otherUserId).success(function(data){
+      $scope.friendsCount = data.length;
+    });
+
     $http.get('/api/user/'+$scope.otherUserId+'/userInfo').success(function(data){
         $scope.userprofile = data;
     });
