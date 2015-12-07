eeab9ecbf0d8f79a8f455919df6f7beecd1f34c0
Perawith <perawithjarunithi@gmail.com>
2015-12-07 21:58:14 +0700

    Add groupfeed&userwall pegination API and refactor contenttype get method

--- newsfeed/urls.py
+++ newsfeed/urls.py
@@ -9,7 +9,10 @@ urlpatterns = [
     url(r'^comment/$', views.CommentViewList.as_view()),
     url(r'^comment/(?P<id>\d+)/$', views.CommentViewDetail.as_view()),
     url(r'^post/(?P<id>\d+)/comment', views.PostComment.as_view()),
-    url(r'^wall/(?P<id>\d+)/$', views.UserWallDetail.as_view()),
+    url(r'^wall/(?P<id>\d+)$', views.UserWallDetail.as_view()),
+    url(r'^wall/(?P<id>\d+)&limit=(?P<limit>\d+)$', views.UserWallDetail.as_view()),
+    url(r'^wall/(?P<id>\d+)/(?P<action>\w+)/(?P<post_id>\d+)$', views.UserWallPegination.as_view()),
+    url(r'^wall/(?P<id>\d+)/(?P<action>\w+)/(?P<post_id>\d+)&limit=(?P<limit>\d+)$', views.UserWallPegination.as_view()),
     url(r'^(?P<action>\w+)/(?P<id>\d+)$', views.PostPagination.as_view()),
     url(r'^(?P<action>\w+)/(?P<id>\d+)&limit=(?P<limit>\d+)$', views.PostPagination.as_view()),
 ]
